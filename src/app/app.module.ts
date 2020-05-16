da7a1a295b729a702d473f61cc570a53aa7ab2cb
IDEA-PC <chakri.mattaparties@hotmail.com>
2020-05-16 21:02:27 +0530

    Created Listing Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { PricingComponent } from './common/pricing/pricing.component';
+import { NgMarqueeModule } from 'ng-marquee';
+import { ChartListComponent } from './chart-list/chart-list.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PricingComponent,
+    ChartListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgMarqueeModule,
+    MatTabsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
